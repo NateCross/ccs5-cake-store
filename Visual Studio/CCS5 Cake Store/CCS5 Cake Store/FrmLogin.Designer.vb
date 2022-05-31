092e7f3d5f39a3c6c7fb19eb52b04ed2db1b1273
NateCross <jonathan.a.b.cross@gmail.com>
2022-05-31 10:00:13 +0800

    Started work on first form

---
+++ Visual Studio/CCS5 Cake Store/CCS5 Cake Store/FrmLogin.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Location = New System.Drawing.Point(655, 245)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(113, 45)
+        Me.BtnLogin.TabIndex = 0
+        Me.BtnLogin.Text = "LOGIN"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'FrmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(918, 302)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Name = "FrmLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnLogin As System.Windows.Forms.Button
+
+End Class
---
+++ Visual Studio/CCS5 Cake Store/CCS5 Cake Store/FrmLogin.vb
@@ -0,0 +1,6 @@
+﻿Public Class FrmLogin
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnLogin.Click
+
+    End Sub
+End Class
